db4e3124c295c20b6cfbf5cfe4ddcebff48e8e91
Jeimmy Kassandra Rojas Carrasco <jrojas@sentra.cl>
2020-07-06 16:36:31 -0400

    Subo cambios de validacion 2 y 4

---
+++ rules/Rule2.go
@@ -0,0 +1,32 @@
+package rules
+
+import (
+	"strings"
+)
+
+//Execute2: Cuando un element tiene nombre "codigoRetorno" o "glosaRetorno" deben tener los valores 
+// minOccurs="1" maxOccurs="1" y El tipo de dato del codigo de retorno debe ser String
+func (r *Rule) Execute2(xsd Schema) {
+	validacionNombre := false
+	validacionString := false
+	validacionOccurs := false
+	for _, complexType := range xsd.ComplexType {
+		for _, element := range complexType.Sequence.Element {
+			if element.Name == "codigoRetorno" || element.Name == "glosaRetorno"{
+				validacionNombre = true
+				if strings.Contains(element.Type, "String"){
+					validacionString = true
+				}
+				if element.MinOccurs == "1" && element.MaxOccurs == "1"{
+					validacionOccurs = true
+				}
+			} 
+		}
+	}
+	if validacionNombre && validacionString && validacionOccurs{
+		r.result = "OK"
+	}else{
+		r.result = "NOK"
+		r.detail = "El elemento no cumple con alguna de las siguientes reglas\n1. Contiene codigoRetorno o glosaRetorno\n2. Valor en 1 en minOccurs y maxOccurs\n3. El tipo de dato debe ser String"
+	}
+}
